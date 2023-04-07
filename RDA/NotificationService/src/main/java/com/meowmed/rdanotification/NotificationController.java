e6785d29292725b4c03ec46cb05624b36c1eaee4
Daniel <daniel.arnold@stud.hs-hannover.de>
2023-04-07 15:03:02 +0200

    NotificationService Grundfunktionalität

--- RDA/NotificationService/src/main/java/com/meowmed/rdanotification/NotificationController.java
+++ RDA/NotificationService/src/main/java/com/meowmed/rdanotification/NotificationController.java
@@ -1,26 +1,28 @@
 package com.meowmed.rdanotification;
 
+import com.meowmed.rdanotification.Email.EmailDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NotificationController {
 
     private final NotificationService nService;
+    @Autowired
     public NotificationController(NotificationService notificationService) {
         this.nService = notificationService;
     }
 
     @PostMapping("/customernotification")
-    public int postNotificationCustomer() {
-        return nService.customerNotification();
-
+    public String postNotificationCustomer(@RequestBody EmailDetails details) {
+        return nService.customerNotification(details);
     }
 
     @PostMapping("/policynotification")
-    public int postNotificationPolicy() {
-        return nService.policyNotification();
+    public String postNotificationPolicy(@RequestBody EmailDetails details) {
+        return nService.policyNotification(details);
     }
 
     
--- RDA/NotificationService/src/main/java/com/meowmed/rdanotification/NotificationService.java
+++ RDA/NotificationService/src/main/java/com/meowmed/rdanotification/NotificationService.java
@@ -1,18 +1,106 @@
 package com.meowmed.rdanotification;
 
+import com.meowmed.rdanotification.Email.EmailDetails;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
+
+@Service
 public class NotificationService {
-
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String sender;
 
     public NotificationService() {}
 
-    public int customerNotification() {
-        return 2;
+    public String customerNotification(EmailDetails details) {
+        MimeMessage mimeMessage
+                = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+
+            // Setting multipart as true for attachments to
+            // be send
+            mimeMessageHelper
+                    = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(details.getRecipient());
+            mimeMessageHelper.setText(details.getMsgBody());
+            mimeMessageHelper.setSubject(
+                    details.getSubject());
+
+            // Adding the attachment
+            FileSystemResource file
+                    = new FileSystemResource(
+                    new File(details.getAttachment()));
+
+            mimeMessageHelper.addAttachment(
+                    file.getFilename(), file);
+
+            // Sending the mail
+            javaMailSender.send(mimeMessage);
+            return "Mail sent Successfully";
+        }
+
+        // Catch block to handle MessagingException
+        catch (MessagingException e) {
+
+            // Display message when exception occurred
+            return "Error while sending mail!!!";
+        }
+
+
     }
-    public int policyNotification() {
-        return 2;
+
+    public String policyNotification(EmailDetails details) {
+        MimeMessage mimeMessage
+                = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+
+            // Setting multipart as true for attachments to
+            // be send
+            mimeMessageHelper
+                    = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(details.getRecipient());
+            mimeMessageHelper.setText(details.getMsgBody());
+            mimeMessageHelper.setSubject(
+                    details.getSubject());
+
+            // Adding the attachment
+            FileSystemResource file
+                    = new FileSystemResource(
+                    new File(details.getAttachment()));
+
+            mimeMessageHelper.addAttachment(
+                    file.getFilename(), file);
+
+            // Sending the mail
+            javaMailSender.send(mimeMessage);
+            return "Mail sent Successfully";
+        }
+
+        // Catch block to handle MessagingException
+        catch (MessagingException e) {
+
+            // Display message when exception occurred
+            return "Error while sending mail!!!";
+        }
+
+
     }
+
 
 
     
