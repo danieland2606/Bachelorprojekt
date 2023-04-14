9aeaa1dc4a7780473ae8c89dfa3b470432866795
Sebastian Porcher <sebastian.porcher@stud.hs-hannover.de>
2023-04-14 12:34:33 +0200

    Port für policy, klassen umbenannt

--- EDA/CustomerService/src/main/java/EDA/MeowMed/MeowMedApplication.java
+++ EDA/CustomerService/src/main/java/EDA/MeowMed/MeowMedApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerApplication {
+public class MeowMedApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
+		SpringApplication.run(MeowMedApplication.class, args);
 	}
 }
