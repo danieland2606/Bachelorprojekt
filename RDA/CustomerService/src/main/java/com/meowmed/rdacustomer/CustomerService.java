026d265c5d0c21feeabd71dd16fa63dafa76f173
legendarsword <github@legendarsword.de>
2023-04-15 12:08:55 +0200

    RDA Notifikation Pom angepasst und Imports gelösch

--- RDA/CustomerService/src/main/java/com/meowmed/rdacustomer/CustomerService.java
+++ RDA/CustomerService/src/main/java/com/meowmed/rdacustomer/CustomerService.java
@@ -9,15 +9,11 @@ import com.meowmed.rdacustomer.entity.MailCustomerEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
