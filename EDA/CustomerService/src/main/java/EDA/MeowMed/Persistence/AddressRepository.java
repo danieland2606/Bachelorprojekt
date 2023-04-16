3e0dcbf588cd9b65eeeef4222d57c4b5f94c8aa8
Tim <tim.van-dorp@stud.hs-hannover.de>
2023-04-16 11:23:12 +0200

    EDA CustomerService AddressRepository gelöscht

--- EDA/CustomerService/src/main/java/EDA/MeowMed/Persistence/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package EDA.MeowMed.Persistence;
-
-import EDA.MeowMed.Persistence.Entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
