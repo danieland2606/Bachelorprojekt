974343dc88a086ed52017a84c9947c1f8cd553b5
Daniel <daniel.arnold@stud.hs-hannover.de>
2023-04-28 11:27:48 +0200

    Anpassung von CustomerEntity durch Einfügung von hasDog

--- RDA/CustomerService/src/main/java/com/meowmed/rdacustomer/entity/CustomerRequest.java
+++ RDA/CustomerService/src/main/java/com/meowmed/rdacustomer/entity/CustomerRequest.java
@@ -1,6 +1,7 @@
 package com.meowmed.rdacustomer.entity;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class CustomerRequest {
 
@@ -15,11 +16,12 @@ public class CustomerRequest {
     private String phoneNumber;
     private String email;
     private String bankDetails;
+    private boolean hasDog;
     public CustomerRequest() {
     }
     public CustomerRequest(String firstName, String lastName, String title, String formOfAdress, String maritalStatus,
             LocalDate dateOfBirth, String employmentStatus, AddressEntity address, String phoneNumber, String email,
-            String bankDetails) {
+            String bankDetails, boolean hasDog) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.title = title;
@@ -31,6 +33,7 @@ public class CustomerRequest {
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.bankDetails = bankDetails;
+        this.hasDog= hasDog;
     }
     public String getFirstName() {
         return firstName;
@@ -98,96 +101,34 @@ public class CustomerRequest {
     public void setBankDetails(String bankDetails) {
         this.bankDetails = bankDetails;
     }
+    public boolean isHasDog() {return hasDog;}
+    public void setHasDog(boolean hasDog) {this.hasDog = hasDog;}
+
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        result = prime * result + ((title == null) ? 0 : title.hashCode());
-        result = prime * result + ((formOfAdress == null) ? 0 : formOfAdress.hashCode());
-        result = prime * result + ((maritalStatus == null) ? 0 : maritalStatus.hashCode());
-        result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
-        result = prime * result + ((employmentStatus == null) ? 0 : employmentStatus.hashCode());
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        result = prime * result + ((bankDetails == null) ? 0 : bankDetails.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomerRequest that)) return false;
+        return hasDog == that.hasDog && firstName.equals(that.firstName) && lastName.equals(that.lastName) && title.equals(that.title) && formOfAdress.equals(that.formOfAdress) && maritalStatus.equals(that.maritalStatus) && dateOfBirth.equals(that.dateOfBirth) && employmentStatus.equals(that.employmentStatus) && address.equals(that.address) && phoneNumber.equals(that.phoneNumber) && email.equals(that.email) && bankDetails.equals(that.bankDetails);
     }
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        CustomerRequest other = (CustomerRequest) obj;
-        if (firstName == null) {
-            if (other.firstName != null)
-                return false;
-        } else if (!firstName.equals(other.firstName))
-            return false;
-        if (lastName == null) {
-            if (other.lastName != null)
-                return false;
-        } else if (!lastName.equals(other.lastName))
-            return false;
-        if (title == null) {
-            if (other.title != null)
-                return false;
-        } else if (!title.equals(other.title))
-            return false;
-        if (formOfAdress == null) {
-            if (other.formOfAdress != null)
-                return false;
-        } else if (!formOfAdress.equals(other.formOfAdress))
-            return false;
-        if (maritalStatus == null) {
-            if (other.maritalStatus != null)
-                return false;
-        } else if (!maritalStatus.equals(other.maritalStatus))
-            return false;
-        if (dateOfBirth == null) {
-            if (other.dateOfBirth != null)
-                return false;
-        } else if (!dateOfBirth.equals(other.dateOfBirth))
-            return false;
-        if (employmentStatus == null) {
-            if (other.employmentStatus != null)
-                return false;
-        } else if (!employmentStatus.equals(other.employmentStatus))
-            return false;
-        if (address == null) {
-            if (other.address != null)
-                return false;
-        } else if (!address.equals(other.address))
-            return false;
-        if (phoneNumber == null) {
-            if (other.phoneNumber != null)
-                return false;
-        } else if (!phoneNumber.equals(other.phoneNumber))
-            return false;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
-        if (bankDetails == null) {
-            if (other.bankDetails != null)
-                return false;
-        } else if (!bankDetails.equals(other.bankDetails))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, title, formOfAdress, maritalStatus, dateOfBirth, employmentStatus, address, phoneNumber, email, bankDetails, hasDog);
     }
     @Override
     public String toString() {
-        return "CustomerRequest [firstName=" + firstName + ", lastName=" + lastName + ", title=" + title
-                + ", formOfAdress=" + formOfAdress + ", maritalStatus=" + maritalStatus + ", dateOfBirth=" + dateOfBirth
-                + ", employmentStatus=" + employmentStatus + ", address=" + address + ", phoneNumber=" + phoneNumber
-                + ", email=" + email + ", bankDetails=" + bankDetails + "]";
+        return "CustomerRequest{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", title='" + title + '\'' +
+                ", formOfAdress='" + formOfAdress + '\'' +
+                ", maritalStatus='" + maritalStatus + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", employmentStatus='" + employmentStatus + '\'' +
+                ", address=" + address +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", bankDetails='" + bankDetails + '\'' +
+                ", hasDog=" + hasDog +
+                '}';
     }
-    
-
 }
