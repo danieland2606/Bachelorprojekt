1f1161f5e8157133d980f29b8da88358e739790e
Mozamil <mozamil.ahmadzaeistud.hs-hannover.de>
2023-05-05 13:31:29 +0200

    Dokumentation für die Exceptions hinzugefügt

--- RDA/PolicyService/src/main/java/com/meowmed/rdapolicy/exceptions/MailSendException.java
+++ RDA/PolicyService/src/main/java/com/meowmed/rdapolicy/exceptions/MailSendException.java
@@ -1,5 +1,29 @@
 package com.meowmed.rdapolicy.exceptions;
-
+/**
+ * Die MailSendException wird ausgelöst, wenn das Versenden einer E-Mail fehlschlägt.
+ */
 public class MailSendException extends RuntimeException{
+   
+    /**
+    * Konstruiert eine neue MailSendException ohne Fehlermeldung.
+    */
+    public MailSendException(){
+    }
+    
+    /**
+     * Konstruiert eine neue MailSendException mit der angegebenen Fehlermeldung
+     * @param message die Fehlermeldung 
+     */
+    public MailSendException(String message) {
+        super(message);
+    }
+
+    /**
+     * gibt die Fehlermeldung der MailSendException zurück
+     * @return die Fehlermeldung
+     */
+    public String getMessage(){
+        return super.getMessage();
+    }
     
 }
--- RDA/PolicyService/src/main/java/com/meowmed/rdapolicy/exceptions/PolicyNotFoundException.java
+++ RDA/PolicyService/src/main/java/com/meowmed/rdapolicy/exceptions/PolicyNotFoundException.java
@@ -1,5 +1,28 @@
 package com.meowmed.rdapolicy.exceptions;
-
+/**
+ * Die PolicyNotFoundException wird ausgelöst, wenn keine Policy gefunden wird.
+ */
 public class PolicyNotFoundException extends RuntimeException{
     
+    /**
+    * Konstruiert eine neue PolicyNotFoundException ohne Fehlermeldung.
+    */
+    public PolicyNotFoundException(){
+    }
+
+    /**
+    * Konstruiert eine neue PolicyNotFoundException mit der angegebenen Fehlermeldung
+    * @param message die Fehlermeldung 
+    */
+    public PolicyNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * gibt die Fehlermeldung der PolicyNotFoundException zurück
+     * @return die Fehlermeldung
+     */
+    public String getMessage(){
+        return super.getMessage();
+    }
 }
