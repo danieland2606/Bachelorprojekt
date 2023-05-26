a41d44cb4d64a45ac8ef49a9701014e375d045e1
Mozamil <mozamil.ahmadzaeistud.hs-hannover.de>
2023-05-26 14:04:30 +0200

    BillingService erstellt

---
+++ RDA/rdabilling/src/main/java/com/meowmed/rdabilling/RdaBillingApplication.java
@@ -0,0 +1,13 @@
+package com.meowmed.rdabilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdaBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdaBillingApplication.class, args);
+	}
+
+}
---
+++ RDA/rdabilling/src/test/java/com/meowmed/rdabilling/RdaBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meowmed.rdabilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdaBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
