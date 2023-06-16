93a4addfa1edfc98d4888e87e0904cb7e5aaa3c1
opitzju <julian.opitz@stud.hs-hannover.de>
2023-06-16 14:34:11 +0200

    UI: Premium convertiert Währung jetzt in einem api call. Symbole für Währungen hizugefügt.

--- UI/routes/api/premium.tsx
+++ UI/routes/api/premium.tsx
@@ -14,13 +14,8 @@ export const handler = {
     const calcPolicy = deserializePolicyCalc(form);
     try {
       const { premium } = await policyClient.calcPolicyPrice(calcPolicy);
-
-      const converted = {
-        eur: premium,
-        usd: await convertCurrency(premium, "USD"),
-        sar: await convertCurrency(premium, "SAR"),
-        btc: await convertCurrency(premium, "BTC"),
-      };
+      const [USD, SAR, BTC] = await convertCurrency(premium);
+      const converted = { EUR: premium, USD, SAR, BTC };
       return respond({ premium: converted });
     } catch (_) {
       return respond({ error: "error" });
@@ -28,19 +23,22 @@ export const handler = {
   },
 };
 
-function round(val: number, places: number) {
+function round(val?: number) {
+  if (val == null) {
+    return NaN;
+  }
   const integer = Math.trunc(val);
   const rest = val - integer;
-  const rounded = Number.parseFloat(rest.toPrecision(places));
+  const rounded = Number.parseFloat(rest.toPrecision(2));
   return integer + rounded;
 }
 
-async function convertCurrency(amount: number, to: string) {
-  const response = await fetch(
-    `https://api.exchangerate.host/convert?from=EUR&to=${to}&amount=${amount}`,
-  );
-  const { result } = await response.json();
-  return round(result, 2);
+async function convertCurrency(amount: number) {
+  const api = `https://api.exchangerate.host/latest` +
+    `?base=EUR&symbols=USD,SAR,BTC&amount=${amount}`;
+  const response = await fetch(api);
+  const { rates: { USD, SAR, BTC } } = await response.json();
+  return [round(USD), round(SAR), round(BTC)];
 }
 
 function respond(premium: Props) {
@@ -51,20 +49,20 @@ function respond(premium: Props) {
 
 interface Props {
   premium?: {
-    eur: number;
-    usd: number;
-    sar: number;
-    btc: number;
+    EUR: number;
+    USD: number;
+    SAR: number;
+    BTC: number;
   };
   error?: string;
 }
 
 export function PremiumDisplay({ premium, error }: Props) {
-  const { eur, usd, sar, btc } = premium ?? {};
+  const { EUR, USD, SAR, BTC } = premium ?? {};
   const btnContainer =
     "absolute flex justify-between transform -translate-y-1/2 left-2 right-2 top-1/2";
   const slide = "carousel-item relative w-full";
-  const btn = "btn btn-circle btn-sm";
+  const btn = "btn btn-circle btn-xs";
   const html = render(
     <html lang="de" data-theme="retro">
       <head>
@@ -76,31 +74,31 @@ export function PremiumDisplay({ premium, error }: Props) {
           ? (
             <div class="carousel w-full">
               <div id="slide1" class={slide}>
-                <p class="m-auto">{eur + "€"}</p>
+                <p class="m-auto">{EUR + "€"}</p>
                 <div class={btnContainer}>
-                  <a href="#slide4" class={btn}>❮</a>
-                  <a href="#slide2" class={btn}>❯</a>
+                  <a href="#slide4" class={btn}>₿</a>
+                  <a href="#slide2" class={btn}>$</a>
                 </div>
               </div>
               <div id="slide2" class={slide}>
-                <p class="m-auto">{usd + "$"}</p>
+                <p class="m-auto">{"$" + USD}</p>
                 <div class={btnContainer}>
-                  <a href="#slide1" class={btn}>❮</a>
-                  <a href="#slide3" class={btn}>❯</a>
+                  <a href="#slide1" class={btn}>€</a>
+                  <a href="#slide3" class={btn}>﷼</a>
                 </div>
               </div>
               <div id="slide3" class={slide}>
-                <p class="m-auto">{sar + "﷼"}</p>
+                <p class="m-auto">{SAR + "﷼"}</p>
                 <div class={btnContainer}>
-                  <a href="#slide2" class={btn}>❮</a>
-                  <a href="#slide4" class={btn}>❯</a>
+                  <a href="#slide2" class={btn}>$</a>
+                  <a href="#slide4" class={btn}>₿</a>
                 </div>
               </div>
               <div id="slide4" class={slide}>
-                <p class="m-auto">{btc + "₿"}</p>
+                <p class="m-auto">{"₿" + BTC}</p>
                 <div class={btnContainer}>
-                  <a href="#slide3" class={btn}>❮</a>
-                  <a href="#slide1" class={btn}>❯</a>
+                  <a href="#slide3" class={btn}>﷼</a>
+                  <a href="#slide1" class={btn}>€</a>
                 </div>
               </div>
             </div>
